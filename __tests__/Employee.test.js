8a396ad873e8213c9fad4b1f6d79457a6fbc3338
Chris Key <robertschris010@gmail.com>
2022-08-06 21:19:03 -0500

    added office number to test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,2 +1,31 @@
-const Emmployee = require('../lib/Employee');
-const employee = new Employee('jester', '12344321', 'jester@gmail.com');
+const Employee = require('../lib/Employee');
+const Employee = require('../lib/Employee');
+const employee = new Employee('jester', '12344321', '511', 'jester@gmail.com');
+
+//we will create test below
+test('creating employee object', () => {
+    expect(employee.name).toBe('jester');
+    expect(employee.id).toBe('12344321');
+    expect(employee.email).toBe('jester@gmail.com');
+});
+
+test ('creating employee id', () => {
+    const employee = new Employee('jester');
+    expect(employee.id).toBe('12344321');
+});
+
+test('createing employee email', () => {
+    const employee = new Employee('');
+
+    expect(employee.email).toBe('jester@gmail.com');
+});
+
+test('creating office number', () => {
+    expect(employee.officeNumber).toBe('511');
+});
+
+test('creating role', () => {
+    expect(employee.getRole).toBe('Employee')
+});
+
+module.exports = Employee;
